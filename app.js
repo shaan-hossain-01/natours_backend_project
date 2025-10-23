1cc03f03dd6952812acce99aa1bd1a3cca512b49
Atiq Afzal Hossain Masud <rockshaan200@gmail.com>
2025-10-23 16:25:18 +0600

    fix: update app.js

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the serverside!', app: `Natours` });
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the serverside!', app: `Natours` });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).json({ message: 'You have sent a POST request' });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.status(200).json({ message: 'You have sent a POST request' });
-});
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
