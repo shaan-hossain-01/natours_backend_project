4d309c3d0053049b5c878fb875bf6b8138a499bf
shaan-hossain-01 <rockshaan200@gmail.com>
2025-11-26 20:25:42 +0600

    started working on the routes

--- app.js
+++ app.js
@@ -3,28 +3,35 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the serverside!', app: `Natours` });
-// });
-
-// app.post('/', (req, res) => {
-//   res.status(200).json({ message: 'You have sent a POST request' });
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
+app.get('/api/v1/tours'
+  , (req, res) => {
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours: tours,
+      },
+    });
   });
-});
+
+app.get('/api/v1/tours/:id'
+  , (req, res) => {
+    console.log(req.params);
+
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === req.params);
+    res.status(200).json({
+      status: 'success',
+      //      results: tours.length,
+      //    data: {
+      //    tours: tours,
+      //},
+    });
+  });
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
